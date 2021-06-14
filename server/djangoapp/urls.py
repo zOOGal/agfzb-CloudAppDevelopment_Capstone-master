30d19e75d5f436d2fd07dfd7a79458b459d52f71
ElinorGX <colgx@outlook.com>
2021-06-14 17:34:49 -0400

    Initial commit

---
+++ server/djangoapp/urls.py
@@ -0,0 +1,31 @@
+from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
+from . import views
+
+app_name = 'djangoapp'
+urlpatterns = [
+    # route is a string contains a URL pattern
+    # view refers to the view function
+    # name the URL
+    path(route='', view=views.get_dealerships, name='index'),
+
+    # path for about view
+    path(route='about/', view=views.about, name='about'),
+
+    # path for contact us view
+    path(route='contact/', view=views.contact, name='contact'),
+
+    # # path for registration
+    # path(route='registration', view=views.registration_request, name='registration'),
+    # # path for login
+    # path(route='login', view=views.login_request, name='login'),
+    # # path for logout
+    # path(route='logout', view=views.logout_request, name='logout'),
+    # path(route='', view=views.get_dealerships, name='index'),
+
+    # path for dealer reviews view
+
+    # path for add a review view
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
